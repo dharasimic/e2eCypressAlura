710584eb44cedd0dc7c5691c04f87d5acaf2d3ad
Dhara Simic <dhara.soares@modalgr.io>
2025-02-24 16:25:56 -0300

    fix: ocultado o token do usuário

--- cypress/e2e/api-perfil.cy.js
+++ cypress/e2e/api-perfil.cy.js
@@ -1,11 +1,10 @@
 describe('Api Adopet', () =>{
-    const authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzMjlkOGZiZi0yMmU2LTQ3ZDMtODMwZS0wZDRkMTRiZTI0YmEiLCJhZG9wdGVyTmFtZSI6Ik1hcmlhIiwiaWF0IjoxNzQwMTY1Nzc5LCJleHAiOjE3NDA0MjQ5Nzl9.kBD9AVlkG8EBOUqQ9DA81TPNJIeLxXMtpelGpXwhfHA'
-    
+   
     it ('API Perfil do Usuário', () => {
         cy.request({
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/adotante/perfil/329d8fbf-22e6-47d3-830e-0d4d14be24ba',
-            headers: {authorization}
+            headers: Cypress.env()
         }).then((res) => {
             expect(res.status).to.be.equal(200)
             expect(res.body).is.not.empty
